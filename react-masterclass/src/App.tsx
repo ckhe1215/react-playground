996a7e67def8efc0102b6829b10e9b756743da22
ckhe1215 <ckhe1215@gmail.com>
2023-05-31 23:48:50 +0900

    react query

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Router from "./Router";
 import { createGlobalStyle } from "styled-components";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
@@ -71,6 +72,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
