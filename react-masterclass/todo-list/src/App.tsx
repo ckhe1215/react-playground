b4b0d41ad94e2e5f2c53c7d05fa2e0a454203309
haeun kim <ckhe1215@gmail.com>
2023-06-03 17:24:33 +0900

    start todo project

---
+++ react-masterclass/todo-list/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
+
+const GlobalStyle = createGlobalStyle`
+
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
+  );
+}
+
+export default App;
