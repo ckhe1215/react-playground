c73c3ffc02a29e61e9b67e2d55372392b3879460
haeun kim <ckhe1215@gmail.com>
2023-06-04 17:06:30 +0900

    create basic to do list

---
+++ react-masterclass/todo-list/src/components/ToDoList.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDo />
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
