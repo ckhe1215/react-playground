cf8379b3c1b6d7186951672e3b70d8c1746e11c3
haeun kim <ckhe1215@gmail.com>
2023-06-10 03:40:03 +0900

    create tasks

--- react-masterclass/trello/src/Components/DraggableCard.tsx
+++ react-masterclass/trello/src/Components/DraggableCard.tsx
@@ -2,28 +2,33 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  background-color: ${(props) =>
+    props.isDragging ? "#74b9ff" : props.theme.cardColor};
   margin-bottom: 10px;
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 5px rgba(0, 0, 0, 0.05)" : "none"};
 `;
 
 interface IDraggabbleCardProps {
-  toDo: string;
+  toDoId: number;
+  toDoText: string;
   index: number;
 }
 
-function DraggableCard({ toDo, index }: IDraggabbleCardProps) {
+function DraggableCard({ toDoId, toDoText, index }: IDraggabbleCardProps) {
   return (
-    <Draggable key={toDo} draggableId={toDo} index={index}>
-      {(provided) => (
+    <Draggable key={toDoId} draggableId={toDoId + ""} index={index}>
+      {(provided, snapshot) => (
         <Card
+          isDragging={snapshot.isDragging}
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          {toDo}
+          {toDoText}
         </Card>
       )}
     </Draggable>
